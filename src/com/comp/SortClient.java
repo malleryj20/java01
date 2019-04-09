1caa4616ce69b06bb7d7845e9b45069db6112e0a
malleryj20 <malleryj20@greenhill.org>
2019-04-09 14:43:07 -0500

    grouping programs

---
+++ src/com/comp/SortClient.java
@@ -0,0 +1,16 @@
+package com.comp;
+
+import java.util.ArrayList;
+
+public class SortClient {
+
+    public static void main(String[] args) {
+        ArrayList x = new ArrayList<Integer>();
+        x.add(4);
+        x.add(12);
+        x.add(5);
+        x.add(3);
+        x.add(7);
+        System.out.println(Sort.mergeSort(x, 5).toString());
+    }
+}
